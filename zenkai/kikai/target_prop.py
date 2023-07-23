c8fb9cb2435708d15f07c2caffad51cdaecca9bc
Short <gshort23@yahoo.com>
2023-07-23 15:02:06 +0900

    remove dx learner because i need to reevaluate it and update aedx learner so it will not train the forward model if necessary

--- zenkai/kikai/target_prop.py
+++ zenkai/kikai/target_prop.py
@@ -52,6 +52,36 @@ class StandardTargetPropNet(nn.Module):
         return y[:len(t)], y[len(t):]
 
 
+class XCatTargetPropNet(nn.Module):
+
+    def __init__(self, base_net: nn.Module):
+        """initializer
+
+        Args:
+            base_net (nn.Module): The base "reverse" network. Must take in the output as an input
+            and predict a variation of the input. Expects module that takes in
+            two inputs
+        """
+        super().__init__()
+        self.base_net = base_net
+
+    def forward(self, x: torch.Tensor, t: torch.Tensor, y: torch.Tensor) -> typing.Tuple[torch.Tensor]:
+        """Calculate the reconstruction of the input
+
+        Args:
+            x (torch.Tensor): The input to the "encoder"
+            t (torch.Tensor): The target for the "encoder"
+            y (torch.Tensor): The output from the "encoder"
+
+        Returns:
+            typing.Tuple[torch.Tensor]: the input predicted by the target, the input predicted by the output
+        """
+        x = torch.cat([x, x])
+        out = torch.cat([t, y])
+        y = self.base_net(x, out)
+        return y[:len(t)], y[len(t):]
+
+
 class TargetPropLoss(Loss):
 
     @abstractmethod
@@ -173,85 +203,13 @@ class TargetPropLearner(LearningMachine):
         return y.out(detach)
 
 
-class DXTargetPropLearner(LearningMachine):
-
-    def __init__(self, net: TargetPropNet, forward_machine: LearningMachine, loss: TargetPropLoss, optim_factory: OptimFactory, assessment_name: str='loss'):
-        """initializer
-
-        Usage:
-        
-        # Typical usage would be to use target prop in the step x method
-        # for a Learning Machine
-        def step_x(self, ...)
-            prop_conn = self.target_prop.prepare_conn(conn, prev_x)
-            x = self.target_prop(prop_conn.step.x).sub(1).detach()
-            self.target_prop.step(prop_conn)
-            conn.step.x = x
-            return conn.step.x.tie_inp()
-
-        Args:
-            net (TargetPropNet): The network to learn the reverse connection
-            loss (TargetPropLoss): The loss function to assess the prediction of the inputs with
-            optim_factory (OptimFactory): The optimizer factory to generate the optim
-        """
-        super().__init__()
-        self.net = net
-        self._forward_machine = forward_machine
-        self.optim_factory = optim_factory
-        self.optim = self.optim_factory(net.parameters())
-        self.loss = loss
-        self.assessment_name = assessment_name
-
-    def prepare_io(self, x: IO, t: IO, y: IO):
-        return IO(x[0], t[0], y[0]), x
-
-    def assess_y(self, y: IO, t: IO, reduction_override: str = None) -> AssessmentDict:
-        return self.loss.assess_dict(
-            tuple(y), t[0], reduction_override
-        )
-    
-    def step(self, x: IO, t: IO, state: State):
-        """
-
-        Args:
-            x (IO): 
-            t (IO): 
-            state (State): 
-        """
-        y = state[self, 'y']
-        self.optim.zero_grad()
-
-        assessment = self.forward_machine.assess(y, IO(x[1], detach=True), detach=False)
-        assessment['loss'].backward()
-        self.optim.step()
-    
-    def step_x(self, x: IO, t: IO, state: State) -> IO:
-        """_summary_
-
-        Args:
-            x (IO): _description_
-            t (IO): _description_
-            state (State): _description_
-
-        Returns:
-            IO: _description_
-        """
-        x = x[0]
-        x = x - x.grad
-        x.grad = None
-
-        return IO(x, detach=True)
-    
-    def forward(self, x: IO, state: State, detach: bool = True) -> IO:
-        x.freshen()
-        dy = state[self, 'dy'] = self.net(*x)
-        y = state[self, 'y'] = IO(x[0].detach() + dy)
-        return y.out(detach)
-
-
 class AEDXTargetPropLearner(LearningMachine):
 
-    def __init__(self, net: TargetPropNet, forward_net: nn.Module, loss: TargetPropLoss, ae_loss: Loss, optim_factory: OptimFactory, assessment_name: str='loss'):
+    def __init__(
+        self, net: TargetPropNet, forward_net: nn.Module, 
+        loss: TargetPropLoss, ae_loss: Loss, optim_factory: OptimFactory, 
+        assessment_name: str='loss', train_forward: bool=True
+    ):
         """initializer
 
         Usage:
@@ -274,10 +232,14 @@ class AEDXTargetPropLearner(LearningMachine):
         self.net = net
         self._forward_net = forward_net
         self.optim_factory = optim_factory
-        self.optim = self.optim_factory(chain(net.parameters(), self._forward_net.parameters()))
+        if train_forward:
+            self.optim = self.optim_factory(chain(net.parameters(), self._forward_net.parameters()))
+        else:
+            self.optim = self.optim_factory(net.parameters())
         self.loss = loss
         self.assessment_name = assessment_name
         self.ae_loss = ae_loss
+        self.train_forward = train_forward
 
     def prepare_io(self, x: IO, t: IO, y: IO):
         return IO(x[0], t[0], y[0]), x
@@ -291,9 +253,9 @@ class AEDXTargetPropLearner(LearningMachine):
         """
 
         Args:
-            x (IO): 
-            t (IO): 
-            state (State): 
+            x (IO): The input
+            t (IO): The output
+            state (State): The learning state
         """
         y = state[self, 'y']
         self.optim.zero_grad()
@@ -322,5 +284,86 @@ class AEDXTargetPropLearner(LearningMachine):
     def forward(self, x: IO, state: State, detach: bool = True) -> IO:
         x.freshen()
         dy = state[self, 'dy'] = self.net(*x)
-        y = state[self, 'y'] = IO(x[0].detach() + dy)
+        print(dy)
+        y = state[self, 'y'] = IO(x[0].detach() + dy[0], x[0].detach() + dy[1])
         return y.out(detach)
+
+
+# class DXTargetPropLearner(LearningMachine):
+
+#     def __init__(self, net: TargetPropNet, forward_machine: LearningMachine, loss: TargetPropLoss, optim_factory: OptimFactory, assessment_name: str='loss'):
+#         """initializer
+
+#         Usage:
+        
+#         # Typical usage would be to use target prop in the step x method
+#         # for a Learning Machine
+#         def step_x(self, ...)
+#             prop_conn = self.target_prop.prepare_conn(conn, prev_x)
+#             x = self.target_prop(prop_conn.step.x).sub(1).detach()
+#             self.target_prop.step(prop_conn)
+#             conn.step.x = x
+#             return conn.step.x.tie_inp()
+
+#         Args:
+#             net (TargetPropNet): The network to learn the reverse connection
+#             loss (TargetPropLoss): The loss function to assess the prediction of the inputs with
+#             optim_factory (OptimFactory): The optimizer factory to generate the optim
+#         """
+#         super().__init__()
+#         self.net = net
+#         self._forward_machine = forward_machine
+#         self.optim_factory = optim_factory
+#         self.optim = self.optim_factory(net.parameters())
+#         self.loss = loss
+#         self.assessment_name = assessment_name
+
+#     def prepare_io(self, x: IO, t: IO, y: IO):
+#         return IO(x[0], t[0], y[0]), x
+
+#     def assess_y(self, y: IO, t: IO, reduction_override: str = None) -> AssessmentDict:
+#         return self.loss.assess_dict(
+#             tuple(y), t[0], reduction_override
+#         )
+    
+#     def step(self, x: IO, t: IO, state: State):
+#         """
+
+#         Args:
+#             x (IO): 
+#             t (IO): 
+#             state (State): 
+#         """
+#         y = IO(state[self, 'y'][0], detach=False)
+#         self.optim.zero_grad()
+
+#         x = self._forward_machine.step_x(y, IO(x[1], detach=True))
+#         self.assess_y()
+
+#         assessment['loss'].backward()
+#         print(p.grad)
+#         self.optim.step()
+    
+#     def step_x(self, x: IO, t: IO, state: State) -> IO:
+#         """_summary_
+
+#         Args:
+#             x (IO): _description_
+#             t (IO): _description_
+#             state (State): _description_
+
+#         Returns:
+#             IO: _description_
+#         """
+#         x = x[0]
+#         x = x - x.grad
+#         x.grad = None
+
+#         return IO(x, detach=True)
+    
+#     def forward(self, x: IO, state: State, detach: bool = True) -> IO:
+#         x.freshen()
+#         dy = state[self, 'dy'] = self.net(*x)
+#         y = state[self, 'y'] = IO(x[0].detach() + dy[0], x[0].detach() + dy[1])
+#         return y.out(detach)
+
