3dfb69e9485bf2264e98c479201f44621eb50b93
Greg Short <shortg29@gmail.com>
2023-07-27 18:30:51 +0900

    add dummy target learner and ae target learner

--- tests/kikai/test_target.py
+++ tests/kikai/test_target.py
@@ -1,28 +1,113 @@
+from zenkai.kaku import IO
+from zenkai.kaku.assess import AssessmentDict
+from zenkai.kaku.io import IO
+from zenkai.kaku.state import State
 from zenkai.kikai import (
-    TargetPropLoss, TargetPropNet, TargetPropLearner,
-    RegTargetPropLoss, StandardTargetPropLoss, StandardTargetPropNet,
-    AEDXTargetPropLearner
+    TargetPropLoss, TargetPropLearner, AETargetPropLearner,
+    AETargetPropStepTheta, StandardTargetPropStepTheta,
+    RegTargetPropLoss, StandardTargetPropLoss,
+    cat_yt, split_yt, cat_z
 )
 from .test_grad import THGradLearnerT1
-from zenkai import ThLoss, itadaki, IO, State
+from zenkai import ThLoss, itadaki, IO, State, NullStepX
 from torch.nn import Linear
 import torch
 from zenkai.utils import get_model_parameters
 
+from torch import nn
 
-class TestStandardTargetPropNet():
+class DummyTargetPropLearner(TargetPropLearner):
 
-    def test_forward_returns_reversed_with_correct_size(self):
-        torch.manual_seed(1)
-        target_prop = StandardTargetPropNet(
-            Linear(4, 2)
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super().__init__()
+        self.linear = nn.Linear(in_features, out_features)
+
+        self._loss = StandardTargetPropLoss(ThLoss("mse"))
+        self._step_theta = StandardTargetPropStepTheta(
+            self, self._loss,
+            itadaki.adam(lr=1e-2)
         )
-        x_t, x_y = target_prop.forward(torch.rand(4, 2), torch.rand(4, 4), torch.rand(4, 4))
-        assert x_t.shape == torch.Size([4, 2])
-        assert x_y.shape == torch.Size([4, 2])
+
+    def step(self, x: IO, t: IO, state: State):
+        return self._step_theta.step(x, t, state)
+    
+    def step_x(self, x: IO, t: IO, state: State) -> IO:
+        return x
+    
+    def assess_y(self, y: IO, t: IO, reduction_override: str = None) -> AssessmentDict:
+        return self._loss.assess_dict(y.totuple(), t[0], reduction_override)
+    
+    def forward(self, x: IO, state: State, release: bool = True) -> IO:
+        x.freshen()
+        x = cat_yt(x)
+        x = self.linear(x)
+        x = split_yt(x)
+        state[self, self.Y_PRE] = x
+        return x.out(release)
 
 
-class TestStandardTargetPropLoss():
+class DummyAETargetPropLearner(AETargetPropLearner):
+
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super().__init__()
+        self.linear = nn.Linear(in_features, out_features)
+        self.rec_linear = nn.Linear(out_features, in_features)
+
+        self._loss = StandardTargetPropLoss(ThLoss("mse"))
+        self._step_theta = AETargetPropStepTheta(
+            self, self._loss, 
+            itadaki.adam(lr=1e-2)
+        )
+
+    def step(self, x: IO, t: IO, state: State):
+        return self._step_theta.step(x, t, state)
+    
+    def step_x(self, x: IO, t: IO, state: State) -> IO:
+        return x
+    
+    def assess_y(self, y: IO, t: IO, reduction_override: str = None) -> AssessmentDict:
+        return self._loss.assess_dict(y.totuple(), t[0], reduction_override)
+    
+    def reconstruct(self, z: IO, state: State, release: bool=True):
+        
+        z = cat_z(z)
+        z = self.rec_linear(z)
+        z = split_yt(z)
+        state[self, self.REC_PRE] = z
+        return z.out(release)
+
+    def forward(self, x: IO, state: State, release: bool = True) -> IO:
+        x.freshen()
+        x = cat_yt(x)
+        x = self.linear(x)
+        x = split_yt(x)
+        state[self, self.Y_PRE] = x
+        return x.out(release)
+
+
+class TestCat:
+
+    def test_cat_yt(self):
+        io = IO(torch.rand(2, 2), torch.rand(2, 2), torch.rand(2, 2))
+        y = cat_yt(io)
+        t = torch.cat([io[1], io[2]])
+        assert (y == t).all()
+
+    def test_cat_z(self):
+        io = IO(torch.rand(2, 2), torch.rand(2, 2))
+        y = cat_z(io)
+        t = torch.cat([io[0], io[1]])
+        assert (y == t).all()
+
+    def test_split_yt(self):
+        
+        yt = torch.rand(4, 2)
+        io = split_yt(yt)
+        assert (io[0] == yt[:2]).all()
+        assert (io[1] == yt[2:]).all()
+
+
+class TestStandardTargetPropLoss:
 
     def test_loss_evaluates_loss_of_y_reconstruction(self):
         torch.manual_seed(1)
@@ -40,7 +125,7 @@ class TestStandardTargetPropLoss():
         assert result.item() == target.item()
 
 
-class TestRegTargetPropLoss():
+class TestRegTargetPropLoss:
 
     def test_loss_evaluates_loss_of_y_reconstruction(self):
         torch.manual_seed(1)
@@ -59,76 +144,31 @@ class TestRegTargetPropLoss():
         assert result.item() == (target.item() + target_reg.item())
 
 
-class TestTargetPropLearner():
-
-    def test_loss_evaluates_loss_of_y_reconstruction(self):
-        torch.manual_seed(1)
-        learner = TargetPropLearner(
-            StandardTargetPropNet(Linear(4, 2)),
-            StandardTargetPropLoss(ThLoss("mse")),
-            itadaki.sgd(lr=1e-2)
-        )
-        base_x = IO(torch.rand(4, 2))
-        base_y = IO(torch.rand(4, 4)) 
-        base_t = IO(torch.rand(4, 4))
-        x, t = learner.prepare_io(base_x, base_t, base_y)
-        state = State()
-        y = learner(x, state)
+class TestAETargetPropLearner:
+    
+    def test_ae_target_prop_weights(self):
+        
+        learner = DummyAETargetPropLearner(3, 4)
+        x = IO(torch.rand(4, 4))
+        t = IO(torch.rand(4, 3))
+        y = IO(torch.rand(4, 3))
+        
+        x_, t_ = learner.prepare_io(x, t, y)
         before = get_model_parameters(learner)
-        learner.step(
-            x, t, state
-        )
-        assert (
-            before != get_model_parameters(learner)
-        ).any()
+        learner.step(x_, t_, State())
+        assert (before != get_model_parameters(learner)).any()
 
 
-class TestDXTargetPropLearner():
+class TestTargetPropLearner:
 
-    def test_loss_evaluates_loss_of_y_reconstruction(self):
-        torch.manual_seed(1)
-        learner = AEDXTargetPropLearner(
-            StandardTargetPropNet(Linear(4, 2)),
-            Linear(2, 4),
-            StandardTargetPropLoss(ThLoss("mse")),
-            ThLoss("mse"),
-            itadaki.sgd(lr=1e-2)
-        )
-        base_x = IO(torch.rand(4, 2))
-        base_y = IO(torch.rand(4, 4)) 
-        base_t = IO(torch.rand(4, 4))
-        x, t = learner.prepare_io(base_x, base_t, base_y)
-        state = State()
-        y = learner(x, state)
+    def test_target_prop_weights(self):
+        
+        learner = DummyTargetPropLearner(3, 4)
+        x = IO(torch.rand(4, 4))
+        t = IO(torch.rand(4, 3))
+        y = IO(torch.rand(4, 3))
+        
+        x_, t_ = learner.prepare_io(x, t, y)
         before = get_model_parameters(learner)
-        learner.step(
-            x, t, state
-        )
-        assert (
-            before != get_model_parameters(learner)
-        ).any()
-
-    def test_forward_does_not_update_if_not_training_forward(self):
-        torch.manual_seed(1)
-        forward_net = Linear(2, 4)
-        learner = AEDXTargetPropLearner(
-            StandardTargetPropNet(Linear(4, 2)),
-            forward_net,
-            StandardTargetPropLoss(ThLoss("mse")),
-            ThLoss("mse"),
-            itadaki.sgd(lr=1e-2),
-            train_forward=False
-        )
-        base_x = IO(torch.rand(4, 2))
-        base_y = IO(torch.rand(4, 4)) 
-        base_t = IO(torch.rand(4, 4))
-        x, t = learner.prepare_io(base_x, base_t, base_y)
-        state = State()
-        y = learner(x, state)
-        before = get_model_parameters(forward_net)
-        learner.step(
-            x, t, state
-        )
-        assert (
-            before == get_model_parameters(forward_net)
-        ).all()
+        learner.step(x_, t_, State())
+        assert (before != get_model_parameters(learner)).any()
