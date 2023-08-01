acadd5979d7438cbdf4d67f36b4a0f1341c47994
Greg Short <shortg29@gmail.com>
2023-08-02 08:34:21 +0900

    add tests for voter populator

--- tests/tansaku/test_populators.py
+++ tests/tansaku/test_populators.py
@@ -1,11 +1,12 @@
 import pytest
 
+import torch
+from torch import nn
 from zenkai import Assessment
 from zenkai.tansaku.core import Individual, Population
 from zenkai.tansaku.populators import (BinaryPopulator, ConservativePopulator,
-                                       GaussianPopulator, RepeatPopulator)
-from zenkai.utils import get_model_parameters
-
+                                       GaussianPopulator, RepeatPopulator, VoterPopulator)
+from zenkai.utils import get_model_parameters, StochasticVoter
 from .fixtures import (binary_individual1, binary_individual2, binary_x,
                        binary_x2, individual1, individual2, individual_model,
                        model1, model2, pop_x1, pop_x2, population1,
@@ -67,3 +68,22 @@ class TestConservativePopulator:
     def test_gaussian_populator_raises_error_if_rate_is_invalid(self, individual1):
         with pytest.raises(ValueError):
             ConservativePopulator(GaussianPopulator(5, 0.5), -1)
+
+
+class TestVoterPopulator:
+
+    def test_voter_populator_populates_correct_count(self):
+
+        voter = StochasticVoter(nn.Sequential(nn.Dropout(0.2), nn.Linear(4, 3)), 6)
+        populator = VoterPopulator(voter, 'x')
+        individual = Individual(x=torch.randn(4, 4))
+        population = populator(individual)
+        assert population['x'].shape == torch.Size([6, 4, 3])
+
+    def test_votes_are_different_for_items_in_population(self):
+
+        voter = StochasticVoter(nn.Sequential(nn.Dropout(0.2), nn.Linear(4, 3)), 6)
+        populator = VoterPopulator(voter, 'x')
+        individual = Individual(x=torch.randn(4, 4))
+        population = populator(individual)
+        assert (population['x'][0] != population['x'][1]).any()
