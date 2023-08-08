f33378b54a004479845849dac102338b7bfc96b8
Greg Short <shortg29@gmail.com>
2023-08-08 19:49:30 +0900

    add tests and fix bugs in tansaku modules

--- tests/tansaku/test_mixers.py
+++ tests/tansaku/test_mixers.py
@@ -62,20 +62,20 @@ class TestBinaryRandCrossOver:
 
     def test_binary_rand_crossover(self):
         
-        mixer = tansaku.KBestElitism(2)
+        mixer = tansaku.BinaryRandCrossOverBreeder(0.5)
+        population1 = tansaku.Population(x=torch.randn(4, 4, 2).sign())
+        population2 = tansaku.Population(x=torch.randn(4, 4, 2).sign())
+        new_population = mixer(population1, population2)
+        assert ((new_population['x'] == -1) | (new_population['x'] == 1)).all()
+
+
+class TestSmoothCrossOver:
+
+    def test_gaussian_rand_crossover(self):
+        
+        mixer = tansaku.SmoothCrossOverBreeder()
         population1 = tansaku.Population(x=torch.rand(4, 4, 2))
         population1.report(Assessment(torch.tensor([0.1, 0.2, 0.8, 1.0])))
         population2 = tansaku.Population(x=torch.rand(4, 4, 2))
         new_population = mixer(population1, population2)
-        assert (new_population.sub[[0, 1]]['x'] == population1.sub[[0, 1]]['x']).all()
-        assert (new_population.sub[[2, 3, 4, 5]]['x'] == population2['x']).all()
-
-    def test_k_best_elitism_chooses_best_with_maximize(self):
-        
-        mixer = tansaku.KBestElitism(2)
-        population1 = tansaku.Population(x=torch.rand(4, 4, 2))
-        population1.report(Assessment(torch.tensor([0.1, 0.2, 0.8, 1.0]), True))
-        population2 = tansaku.Population(x=torch.rand(4, 4, 2))
-        new_population = mixer(population1, population2)
-        assert (new_population.sub[[0, 1]]['x'] == population1.sub[[3, 2]]['x']).all()
-        assert (new_population.sub[[2, 3, 4, 5]]['x'] == population2['x']).all()
+        assert new_population['x'].shape == population1['x'].shape
