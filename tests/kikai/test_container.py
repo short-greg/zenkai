7421adb0431174c352542a436e3fdb48be164d66
Greg Short <gshort23@yahoo.com>
2023-11-15 15:17:04 +0900

    make graph easier to use by adding in nodes

--- tests/kikai/test_container.py
+++ tests/kikai/test_container.py
@@ -11,58 +11,57 @@ class SampleGraph(containers.GraphLearner):
     def __init__(self, step_priority: bool = False, target_out: bool = False):
 
         super().__init__()
-        self.linear1 = THGradLearnerT1(8, 4)
-        self.linear2 = THGradLearnerT1(4, 4)
-        self.linear3 = THGradLearnerT1(4, 4)
+        if target_out:
+            target = 't'
+        else: target = None
+        self.linear1 = self.node(THGradLearnerT1(8, 4), target, step_priority)
+        self.linear2 = self.node(THGradLearnerT1(4, 4), target, step_priority)
+        self.linear3 = self.node(THGradLearnerT1(4, 4), step_priority=step_priority)
         self.step_priority = step_priority
         self.target_out = target_out
 
     def assess_y(self, y: IO, t: IO, reduction_override: str = None) -> Assessment:
         return self.linear3.assess_y(y, t, reduction_override)
 
-    def forward_step(
+    def forward(
         self, x: IO, state: State, release: bool = True, *args, **kwargs
     ) -> typing.Iterator[SStep]:
 
-        target = "t" if self.target_out else None
-        y1 = self.linear1(x, state, release)
-        yield self.wrap(self.linear1, x, y1, self.step_priority, target)
-
-        y2 = self.linear2(y1, state, release)
-        yield self.wrap(self.linear2, y1, y2, self.step_priority, target)
-
-        y3 = self.linear3(y2, state, release)
-        yield self.wrap(self.linear3, y2, y3, self.step_priority)
+        base_x = x
+        x = self.linear1(x, state, release, base_x)
+        x = self.linear2(x, state, release, base_x)
+        x = self.linear3(x, state, release, base_x)
+        return x
 
 
 class SampleAccGraph(containers.AccGraphLearner):
     def __init__(self, target_out: bool = False):
 
         super().__init__()
-        self.linear1 = THGradLearnerT1(8, 4)
-        self.linear2 = THGradLearnerT1(4, 4)
-        self.linear3 = THGradLearnerT1(4, 4)
+        if target_out:
+            target = 't'
+        else: target = None
+        self.linear1 = self.node(THGradLearnerT1(8, 4), target)
+        self.linear2 = self.node(THGradLearnerT1(4, 4), target)
+        self.linear3 = self.node(THGradLearnerT1(4, 4))
         self.target_out = target_out
 
     def assess_y(self, y: IO, t: IO, reduction_override: str = None) -> Assessment:
         return self.linear3.assess_y(y, t, reduction_override)
 
-    def forward_step(
+    def forward(
         self, x: IO, state: State, release: bool = True, *args, **kwargs
     ) -> typing.Iterator[SStep]:
 
-        target = "t" if self.target_out else None
-        y1 = self.linear1(x, state, release)
-        yield self.wrap(self.linear1, x, y1, target)
-
-        y2 = self.linear2(y1, state, release)
-        yield self.wrap(self.linear2, y1, y2, target)
-
-        y3 = self.linear3(y2, state, release)
-        yield self.wrap(self.linear3, y2, y3)
+        base_x = x
+        x = self.linear1(x, state, release, base_x)
+        x = self.linear2(x, state, release, base_x)
+        x = self.linear3(x, state, release, base_x)
+        return x
 
 
 class TestGraph:
+
     def test_forward_step_produces_output_of_correct_size(self):
 
         x = IO(torch.rand(4, 8))
@@ -138,18 +137,6 @@ class TestAccGraph:
         graph = SampleAccGraph()
         y = graph(x)
         assert y.f.shape == torch.Size([4, 4])
-
-    def test_accumulate_updates_the_gradients(self):
-
-        x = IO(torch.rand(4, 8))
-        t = IO(torch.rand(4, 4))
-        graph = SampleAccGraph()
-        state = State()
-        graph(x, state)
-        before = get_model_grads(graph)
-        graph.accumulate(x, t, state)
-
-        assert before != get_model_grads(graph)
 
     def test_step_updates_the_parameters(self):
 
--- zenkai/kikai/_containers.py
+++ zenkai/kikai/_containers.py
@@ -4,8 +4,44 @@ from dataclasses import dataclass
 from abc import abstractmethod
 from collections import OrderedDict
 
+# 3rd party
+from torch import nn
+
 # local
 from ..kaku import State, IO, LearningMachine, Assessment
+
+
+class GraphNode(nn.Module):
+
+    def __init__(
+        self, graph: 'GraphLearner', learner: LearningMachine, step_priority: bool=False, 
+        target: typing.Union[str, LearningMachine]=None
+    ):
+
+        super().__init__()
+        self._graph = graph
+        self._learner = learner
+        self._target = target
+        self._step_priority = step_priority
+    
+    def forward(
+        self, x: IO, state: State, release: bool=True, 
+        base_x: IO=None, target: typing.Union[str, LearningMachine]=False, *args, **kwargs
+    ):
+        if target is False:
+            target = self._target
+        
+        y = self._learner(x, state, release, *args, **kwargs)
+
+        if base_x is not None:
+            self._graph.add_step(x, SStep(self._learner, x, y, self._step_priority, target), state)
+        return y
+
+    def __str__(self) -> str:
+        return f'GraphNode {type(self._learner), type(self._target)}'
+    
+    def __repr__(self):
+        return f'GraphNode {type(self._learner), type(self._target)}'
 
 
 @dataclass
@@ -19,24 +55,11 @@ class SStep:
     x_prime: IO = None
 
 
-class GraphLearner(LearningMachine):
-    
+class GraphLearnerBase(LearningMachine):
+
     @abstractmethod
-    def forward_step(
-        self, x: IO, state: State, *args, **kwargs
-    ) -> typing.Iterator[SStep]:
-        pass
-
     def forward(self, x: IO, state: State, release: bool = True, *args, **kwargs) -> IO:
-
-        steps = [step for step in self.forward_step(x, state, *args, **kwargs)]
-        y = steps[-1].y
-        ordered_steps = OrderedDict()
-        for step in steps:
-            ordered_steps[step.machine] = step
-        state[self, x, "steps"] = steps
-        state[self, x, "step_dict"] = ordered_steps
-        return y.out(release)
+        pass
 
     def get_t(self, step: SStep, step_dict, prev_t: IO, t: IO):
 
@@ -46,25 +69,38 @@ class GraphLearner(LearningMachine):
             return t
         return prev_t
 
+    def add_step(self, x: IO, sstep: SStep, state: State):
+
+        steps = state.get_or_set((self, x, 'steps'), [])
+        step_dict = state.get_or_set((self, x, 'step_dict'), OrderedDict())
+        
+        step_dict[sstep.machine] = sstep
+        steps.append(sstep)
+    
+    def get_steps(self, x: IO, state: State, validate: bool=False) -> typing.Tuple[typing.List[SStep], typing.Dict[str, SStep]]:
+
+        steps, step_dict = state.get((self, x, 'steps')), state.get((self, x, 'step_dict'))
+
+        if validate and steps is None:
+            raise RuntimeError(
+                'Cannot step as the steps have not been set. Must pass the x input into the graph into each nodes base_x.')
+
+        return steps, step_dict
+
+    def node(self, learner: LearningMachine, target=None, step_priority: bool=False):
+
+        return GraphNode(self, learner, step_priority, target)
+
     @abstractmethod
     def assess_y(self, y: IO, t: IO, reduction_override: str = None) -> Assessment:
         pass
 
-    def wrap(
-        self,
-        machine: LearningMachine,
-        x: IO,
-        y: IO,
-        step_priority: bool,
-        target: typing.Union[LearningMachine, str] = None,
-    ) -> SStep:
-        return SStep(machine, x, y, step_priority, target)
+
+class GraphLearner(GraphLearnerBase):
 
     def step(self, x: IO, t: IO, state: State):
 
-        steps: typing.List[SStep] = state[self, x, "steps"]
-        step_dict: typing.Dict[LearningMachine, SStep] = state[self, x, "step_dict"]
-
+        steps, step_dict = self.get_steps(x, state, validate=True)
         prev_t = t
         for step in reversed(steps):
             machine = step.machine
@@ -82,54 +118,21 @@ class GraphLearner(LearningMachine):
             prev_t = step.x_prime
 
     def step_x(self, x: IO, t: IO, state: State) -> IO:
-        step: SStep = state[self, x, "steps"][0]
-        return step.x_prime
+        steps, _ = self.get_steps(x, state, True)
+        return steps[0].x_prime
 
 
-class AccGraphLearner(LearningMachine):
-
-    @abstractmethod
-    def forward_step(
-        self, x: IO, state: State, *args, **kwargs
-    ) -> typing.Iterator[SStep]:
-        pass
-
-    def forward(self, x: IO, state: State, release: bool = True, *args, **kwargs) -> IO:
-
-        steps = [step for step in self.forward_step(x, state, *args, **kwargs)]
-        y = steps[-1].y
-        ordered_steps = OrderedDict()
-        for step in steps:
-            ordered_steps[step.machine] = step
-        state[self, x, "steps"] = steps
-        state[self, x, "step_dict"] = ordered_steps
-        return y.out(release)
-
-    def get_t(self, step: SStep, step_dict, prev_t: IO, t: IO):
-
-        if isinstance(step.target, LearningMachine):
-            return step_dict[step.target].x_prime
-        elif step.target == "t":
-            return t
-        return prev_t
+class AccGraphLearner(GraphLearnerBase):
 
     @abstractmethod
     def assess_y(self, y: IO, t: IO, reduction_override: str = None) -> Assessment:
         pass
 
-    def wrap(
-        self,
-        machine: LearningMachine,
-        x: IO,
-        y: IO,
-        target: typing.Union[LearningMachine, str] = None,
-    ) -> SStep:
-        return SStep(machine, x, y, target=target)
-
     def accumulate(self, x: IO, t: IO, state: State):
 
-        steps: typing.List[SStep] = state[self, x, "steps"]
-        step_dict: typing.Dict[LearningMachine, SStep] = state[self, x, "step_dict"]
+        steps, step_dict = self.get_steps(x, state, validate=True)
+        if steps is None:
+            raise RuntimeError('Cannot ')
 
         prev_t = t
         for step in reversed(steps[1:]):
@@ -144,9 +147,7 @@ class AccGraphLearner(LearningMachine):
 
     def step(self, x: IO, t: IO, state: State):
 
-        steps: typing.List[SStep] = state[self, x, "steps"]
-        step_dict: typing.Dict[LearningMachine, SStep] = state[self, x, "step_dict"]
-
+        steps, step_dict = self.get_steps(x, state, True)
         prev_t = t
         for step in reversed(steps):
             machine = step.machine
@@ -155,7 +156,7 @@ class AccGraphLearner(LearningMachine):
             prev_t = step.x_prime
 
     def step_x(self, x: IO, t: IO, state: State) -> IO:
-        steps: typing.List[SStep] = state[self, x, "steps"]
+        steps, step_dict = self.get_steps(x, state, True)
         prev_t = t if len(steps) == 1 else steps[1].x_prime
-        t_i = self.get_t(steps[0], state[self, x, "step_dict"], prev_t, t)
+        t_i = self.get_t(steps[0], step_dict, prev_t, t)
         return steps[0].machine.step_x(steps[0].x, t_i, state)
