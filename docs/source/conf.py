c60826873d263002a21200f64ebe91e7db00cde5
Greg Short <gshort23@yahoo.com>
2023-11-20 10:14:35 +0900

    update version in readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Greg Short"
 author = "Greg Short"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.0.2"
 
 # -- General configuration ---------------------------------------------------
 
