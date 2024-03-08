506e11e8e5d7a56f4126f4e4d372822d4d11b8c1
Short <gshort23@yahoo.com>
2024-03-08 17:02:26 +0900

    add documentation to batchmemory

--- zenkai/utils/_memory.py
+++ zenkai/utils/_memory.py
@@ -4,15 +4,26 @@ import typing
 import numpy as np
 
 
+# for reinforcement learning => would probably do some modifications
+
 class BatchMemory(object):
 
     def __init__(self, samples: typing.List[str], singular: typing.List[str]=None):
+        """Create tensor based memory to 
+
+        Args:
+            samples (typing.List[str]): The names of the batch samples
+            singular (typing.List[str], optional): The name sof any other data constant across the batch sample. Defaults to None.
+
+        Raises:
+            ValueError: If there is overlap between the names listed for singular and for samples
+        """
         
         singular = singular or []
-        if set(samples).intersection(set(singular)) == 0:
-            raise ValueError(
-                'Names of keys for samples and singulars must not overlap and at least one name must be specified'
-            )
+        # if set(samples).intersection(set(singular)) == 0:
+        #     raise ValueError(
+        #         'Names of keys for samples and singulars must not overlap and at least one name must be specified'
+        #     )
         self._samples = {}
         self._singular = {}
         for name in samples:
@@ -23,21 +34,38 @@ class BatchMemory(object):
         self._idx = 0
         self._batch_count = None
 
-    def _cat_if(self, cur: torch.Tensor, present: torch.Tensor=None):
+    def _cat_if(self, new_value: torch.Tensor, cur_value: torch.Tensor=None) -> torch.Tensor:
+        """helper function for updating tensors to handle the case where no tensors have been added yet more elegantly
 
-        if present is None:
-            return cur
-        return torch.cat([present, cur])
+        Args:
+            new_value (torch.Tensor): The new value to concatenate
+            cur_value (torch.Tensor, optional): The curre_description_. Defaults to None.
+
+        Returns:
+            torch.Tensor: The concatenated tensor
+        """
+
+        if cur_value is None:
+            return new_value.detach()
+        return torch.cat([cur_value.detach(), new_value.detach()])
 
     def add_batch(
         self, **kwargs
     ):
+        """Add values to the batch. You must add values for 
+        all of the keys specified in samples and singular
+
+        Raises:
+            RuntimeError: The batch sizes are not the same
+            for all samples
+        """
+
         # 1) validate n_samples
         # 2) validate names
         # 3) 
         n_batch = None
         for name, sample in self._samples.items():
-            cur_sample = kwargs[name].detach()
+            cur_sample = kwargs[name]
             if n_batch is None:
                 n_batch = len(cur_sample)
             else:
@@ -50,7 +78,7 @@ class BatchMemory(object):
         for name, singular in self._singular.items():
 
             self._singular[name] = self._cat_if(
-                kwargs[name][None].detach(), singular
+                kwargs[name][None], singular
             )
         
         self._order = self._cat_if(
@@ -64,6 +92,11 @@ class BatchMemory(object):
         self._idx += 1
     
     def remove_batch(self, idx: int):
+        """Remove a batch from memory
+
+        Args:
+            idx (int): The index of the memory to remove
+        """
 
         to_keep = self._order[self._order != idx]
         self._order[self._order > idx] -= 1
@@ -80,6 +113,11 @@ class BatchMemory(object):
         self._idx -= 1
 
     def remove_samples(self, idx):
+        """Specify samples to remove from memory
+
+        Args:
+            idx: The index or indices to remove
+        """
 
         chosen = torch.zeros(len(self), dtype=bool)
         chosen[idx] = True
@@ -108,11 +146,28 @@ class BatchMemory(object):
             self._batch_count = self._batch_count[zero_bin]
 
     def remove_random_samples(self, n: int):
+        """Randomly choose to remove samples from memory
+
+        Args:
+            n (int): The number of samples to remove
+
+        """
 
         chosen = torch.randperm(len(self._order))[:n]
-        return self.remove_samples(chosen)
+        self.remove_samples(chosen)
 
     def __getitem__(self, idx) -> typing.Dict[str, torch.Tensor]:
+        """Retrieve memories. 
+
+        Args:
+            idx: The index or indices to retrieve
+
+        Raises:
+            ValueError: If the index is invalid
+
+        Returns:
+            typing.Dict[str, torch.Tensor]: A dictionary of the retrieved values and tensors. Note that any singular item 
+        """
 
         if isinstance(idx, typing.Tuple):
             idx = torch.LongTensor(idx)
@@ -130,144 +185,27 @@ class BatchMemory(object):
         return result
     
     def random_sample(self, n: int) -> typing.Dict[str, torch.Tensor]:
+        """Randomly sample a set of tensors
+
+        Args:
+            n (int): The number of samples to retrieve
+
+        Returns:
+            typing.Dict[str, torch.Tensor]: 
+        """
 
         return self[torch.randperm(len(self))[:n]]
 
     def __len__(self) -> int:
+        """
+        Returns:
+            int: The number of samples
+        """
         return len(self._order) if self._order is not None else 0
-    
-    @property
-    def idx(self) -> int:
+
+    def n_batches(self) -> int:
+        """
+        Returns:
+            int: The number of batches
+        """
         return self._idx
-
-
-# class TensorMemory(object):
-
-#     @abstractmethod
-#     def record(self, **kwargs: torch.Tensor):
-#         pass
-
-#     @abstractmethod
-#     def purge_random(self, p: float):
-#         pass
-
-#     @abstractmethod
-#     def purge_count(self, n: int, ascending: bool=True):
-#         pass
-
-#     @abstractmethod
-#     def sample(self, n: int) -> typing.Dict[str, torch.Tensor]:
-#         pass
-
-#     @abstractmethod
-#     def __len__(self) -> int:
-#         pass
-
-
-# # how to deal with non-batch tensors. 
-# # memory.sample()
-
-# class DictTensorMemory(TensorMemory):
-
-#     def __init__(self, batch_tensors: typing.Dict=None, other_tensors: typing.Dict=None, clone: bool=False):
-
-#         batch_tensors = batch_tensors or {}
-#         other_tensors = other_tensors or {}
-#         self._keys = set(batch_tensors.keys()).add(other_tensors.keys())
-#         self._batch_size = None
-#         for k, v in batch_tensors.items():
-#             self._batch_tensors[k] = v.clone() if clone else v.detach()
-#             if self._batch_size is None:
-#                 self._batch_size = len(v)
-#             else:
-#                 if self._batch_size != len(v):
-#                     raise ValueError(f'Incompatible batch sizes of {self._batch_size} and {len(v)}')
-#         self._other_tensors = {
-#             k: v.clone() if clone else v.deatch()
-#             for k, v in other_tensors
-#         }
-    
-#     def __getitem__(self, i: int) -> typing.Dict[str, torch.Tensor]:
-
-#         result = {}
-#         for k, v in self._batch_tensors.items():
-#             result[k] = v[i]
-        
-#         for k, v in self._other_tensors.items():
-#             result[k] = v
-#         return result
-
-#     def __len__(self) -> int:
-#         return self._batch_size
-    
-#     def keys(self) -> typing.Set:
-#         return self._keys
-
-
-# class TensorMemoryStorage(object):
-
-#     def __init__(self):
-
-#         self._n_memories = 0
-#         self._memory_map = {}
-#         self._keys = None
-#         self._memories = []
-
-#     def validate(self, memory: TensorMemory):
-
-#         if self._n_memories != 0 and memory.keys() != self.keys():
-#             raise ValueError(f'')
-
-#     def add(self, memory: TensorMemory):
-        
-#         if self._keys is None:
-#             self._keys = memory.keys()
-#         self._n_memories += len(memory)
-#         self._memories.append(memory)
-
-#     def purge_random(self, n: int):
-#         sampled = set(np.random.choice(n, len(self), True))
-#         memories = []
-#         for i, memory in enumerate(self._memories):
-#             if i not in sampled:
-#                 memories.append(memory)
-#         return memories
-
-#     def purge_count(self, n: int, from_start: bool=True):
-        
-#         if from_start:
-#             self._memories = self._memories[:n]
-#         else:
-#             self._memories = self._memories[-1:-n]
-
-#     def _cat_memories(self, memories) -> typing.Dict[str, torch.Tensor]:
-
-#         preceding = {}
-#         result = {}
-#         for memory in memories:
-
-#             for k, v in memory.items():
-#                 if k in preceding:
-#                     preceding[k] = []
-#                 else:
-#                     preceding[k].append(v)
-
-#         for k, v in preceding.items():
-#             result[k] = torch.stack(v)
-#         return result
-
-#     def sample(self, n: int) -> typing.Dict[str, torch.Tensor]:
-        
-#         sampled = np.random.choice(n, len(self), True)
-        
-#         result = {}
-#         for i in sampled:
-#             cur = np.random.choice(len(self._memory[i]), 1)
-#             result.append(
-#                 self._memory[cur]
-#             )
-#         return self._cat_memories(result)
-        
-#     @abstractmethod
-#     def __len__(self) -> int:
-#         return len(self._memories)
