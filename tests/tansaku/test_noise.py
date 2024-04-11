87f1dafd1d61b73a7f784ac6f1b1c1fb37432900
Greg Short <gshort23@yahoo.com>
2024-04-11 17:38:49 +0900

    remove commented out code

--- tests/tansaku/test_noise.py
+++ tests/tansaku/test_noise.py
@@ -1,14 +1,7 @@
-# 
-import typing
-
 # 3rd party
 import torch
 
 # local
-from zenkai.tansaku import _noise
-from zenkai import kaku
-
-from functools import partial
 import pytest
 import torch
 import torch.nn as nn
@@ -57,60 +50,6 @@ def x_trial_collapsed():
 @pytest.fixture
 def noise():
     return torch.rand(N_SAMPLES, 2, generator=g(3))
-
-
-class TestGaussianNoiser:
-
-    pass
-
-    # def test_gaussian_mapper_generates_gaussian_population_after_one(self):
-
-    #     population = torch.randn(8, 4)
-    #     child = population.apply(partial(_noise.gausian_noise, std=0.1))
-    #     assert child["x"].shape == torch.Size([8, 4])
-
-    # def test_gaussian_mapper_generates_gaussian_population_after_one_with_std_and_mu(
-    #     self,
-    # ):
-
-    #     population = kaku.Population(x=torch.randn(8, 4))
-    #     child = population.apply(partial(_noise.gausian_noise, std=0.1, mean=0.1))
-    #     assert child["x"].shape == torch.Size([8, 4])
-
-    # def test_gaussian_mapper_generates_gaussian_population_after_two_updates(self):
-
-    #     population = kaku.Population(x=torch.randn(8, 4))
-    #     population2 = kaku.Population(x=torch.randn(8, 4))
-    #     child = population.apply(partial(_noise.gausian_noise, std=0.1, mean=0.1))
-    #     child = population2.apply(partial(_noise.gausian_noise, std=0.1, mean=0.1))
-    #     assert child["x"].shape == torch.Size([8, 4])
-
-
-class TestBinarySampler:
-
-    pass
-    # def test_binary_mapper_generates_population_after_one(self):
-
-    #     population = kaku.Population(x=torch.rand(8, 4).round())
-    #     child = population.apply(partial(_noise.binary_noise, flip_p=0.5))
-    #     assert child["x"].shape == torch.Size([8, 4])
-
-    # def test_binary_mapper_generates_population_after_two(self):
-
-    #     population = kaku.Population(x=torch.rand(8, 4).round())
-    #     population2 = kaku.Population(x=torch.rand(8, 4).round())
-    #     child = population.apply(partial(_noise.binary_noise, flip_p=0.5))
-    #     child = population2.apply(partial(_noise.binary_noise, flip_p=0.5))
-    #     assert child["x"].shape == torch.Size([8, 4])
-
-    # def test_binary_mapper_generates_population_after_two_with_signed_neg(self):
-
-    #     population = kaku.Population(x=torch.randn(8, 4).sign())
-    #     population2 = kaku.Population(x=torch.randn(8, 4).sign())
-    #     child = population.apply(partial(_noise.binary_noise, flip_p=0.5))
-    #     child = population2.apply(partial(_noise.binary_noise, flip_p=0.5))
-    #     assert child["x"].shape == torch.Size([8, 4])
-    #     assert ((child["x"] == 1.0) | (child["x"] == -1.0)).all()
 
 
 
@@ -373,3 +312,58 @@ class TestModuleNoise:
         noiser.update(linear2)
         y2 = noiser(torch.randn(24, 4))
         assert y.shape == y2.shape
+
+
+class TestGaussianNoiser:
+
+    pass
+
+    # def test_gaussian_mapper_generates_gaussian_population_after_one(self):
+
+    #     population = torch.randn(8, 4)
+    #     child = population.apply(partial(_noise.gausian_noise, std=0.1))
+    #     assert child["x"].shape == torch.Size([8, 4])
+
+    # def test_gaussian_mapper_generates_gaussian_population_after_one_with_std_and_mu(
+    #     self,
+    # ):
+
+    #     population = kaku.Population(x=torch.randn(8, 4))
+    #     child = population.apply(partial(_noise.gausian_noise, std=0.1, mean=0.1))
+    #     assert child["x"].shape == torch.Size([8, 4])
+
+    # def test_gaussian_mapper_generates_gaussian_population_after_two_updates(self):
+
+    #     population = kaku.Population(x=torch.randn(8, 4))
+    #     population2 = kaku.Population(x=torch.randn(8, 4))
+    #     child = population.apply(partial(_noise.gausian_noise, std=0.1, mean=0.1))
+    #     child = population2.apply(partial(_noise.gausian_noise, std=0.1, mean=0.1))
+    #     assert child["x"].shape == torch.Size([8, 4])
+
+
+class TestBinarySampler:
+
+    pass
+    # def test_binary_mapper_generates_population_after_one(self):
+
+    #     population = kaku.Population(x=torch.rand(8, 4).round())
+    #     child = population.apply(partial(_noise.binary_noise, flip_p=0.5))
+    #     assert child["x"].shape == torch.Size([8, 4])
+
+    # def test_binary_mapper_generates_population_after_two(self):
+
+    #     population = kaku.Population(x=torch.rand(8, 4).round())
+    #     population2 = kaku.Population(x=torch.rand(8, 4).round())
+    #     child = population.apply(partial(_noise.binary_noise, flip_p=0.5))
+    #     child = population2.apply(partial(_noise.binary_noise, flip_p=0.5))
+    #     assert child["x"].shape == torch.Size([8, 4])
+
+    # def test_binary_mapper_generates_population_after_two_with_signed_neg(self):
+
+    #     population = kaku.Population(x=torch.randn(8, 4).sign())
+    #     population2 = kaku.Population(x=torch.randn(8, 4).sign())
+    #     child = population.apply(partial(_noise.binary_noise, flip_p=0.5))
+    #     child = population2.apply(partial(_noise.binary_noise, flip_p=0.5))
+    #     assert child["x"].shape == torch.Size([8, 4])
+    #     assert ((child["x"] == 1.0) | (child["x"] == -1.0)).all()
+
