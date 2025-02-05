bfe9109fd9be32b891503f18b1bf286474f09724
Short <gshort23@yahoo.com>
2025-02-06 08:24:53 +0900

    remove old param code and improve organization

--- tests/utils/test_params.py
+++ tests/utils/test_params.py
@@ -2,9 +2,7 @@
 import torch
 import torch.nn as nn
 
-
 from zenkai.utils import _params as p_utils
-from zenkai.utils._reshape import undo_cat1d, cat1d
 
 
 class TestSetModelParameters:
@@ -17,79 +15,6 @@ class TestSetModelParameters:
         assert (
             p_utils.get_params(mod1) == p_utils.get_params(mod2)
         ).all()
-
-
-# class TestSetModelGrads:
-
-#     def test_set_model_grads_sets_correctly(self):
-
-#         mod1 = nn.Linear(2, 4)
-#         grads = torch.randn(12)
-
-#         p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads), False)
-#         new_grads = p_utils.get_model_grads(mod1)
-
-#         assert (cat1d(new_grads) == grads).all()
-
-#     def test_set_model_grads_adds_grads_when_set_to_true(self):
-
-#         mod1 = nn.Linear(2, 4)
-#         grads1 = torch.randn(12)
-#         grads2 = torch.randn(12)
-
-#         p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads1), False)
-        
-#         new_grads = p_utils.get_model_grads(
-#             mod1, flat_cat=True
-#         )
-#         p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads2), True)
-#         new_grads = p_utils.get_model_grads(
-#             mod1, flat_cat=True
-#         )
-#         assert torch.isclose(cat1d(new_grads), (grads1 + grads2)).all()
-
-#     def test_set_model_grads_does_not_add_when_not_set_to_true(self):
-
-#         mod1 = nn.Linear(2, 4)
-#         grads1 = torch.randn(12)
-#         grads2 = torch.randn(12)
-
-#         p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads1), False)
-#         p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads2), False)
-#         new_grads = p_utils.get_model_grads(mod1)
-#         assert (cat1d(new_grads) == grads2).all()
-
-#     def test_undo_grads_resets_grads(self):
-
-#         mod1 = nn.Linear(2, 4)
-#         grads1 = torch.randn(12)
-#         p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads1), False)
-
-#         with p_utils.undo_grad([mod1]):
-#             grads2 = torch.randn(12)
-#             p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads2), False)
-
-#         new_grads = p_utils.get_model_grads(mod1)
-#         assert (cat1d(new_grads) == grads1).all()
-
-#     def test_undo_grads_resets_grads_for_tensor(self):
-
-#         mod1 = nn.Linear(2, 4)
-#         grads1 = torch.randn(12)
-#         t = torch.rand(12)
-#         t_grad = torch.rand(12)
-#         t.grad = t_grad
-
-#         p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads1), False)
-
-#         with p_utils.undo_grad([mod1, t]):
-#             grads2 = torch.randn(12)
-#             p_utils.update_model_grads(mod1, undo_cat1d(mod1, grads2), False)
-#             t.grad = torch.rand(12)
-
-#         new_grads = p_utils.get_model_grads(mod1)
-#         assert (cat1d(new_grads) == grads1).all()
-#         assert (t_grad == t.grad).all()
 
 
 class TestGetP:
